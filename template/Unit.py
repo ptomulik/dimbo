6256672a83f95b16bf7ace58964a9b41ccf5aef5
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2014-02-10 10:59:39 +0100

    replaced Copyright ... with @COPYRIGHT@[D

--- template/Unit.py
+++ template/Unit.py
@@ -4,7 +4,7 @@ TODO: Write docs for XXX
 """
 
 #
-# Copyright (c) 2012 by Pawel Tomulik
+# @COPYRIGHT@
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
